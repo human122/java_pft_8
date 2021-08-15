6d8f1233267fcfa681c27442154ae646c8685e1c
human122 <zorry@rambler.ru>
2021-08-15 17:45:02 +0300

    реализованы проверка и обеспечение предусловий в тестах, по состоянию задания №8, добавлена модификация контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,11 +2,28 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase {
 
     @Test
     public void testContactModification() {
+        if (! app.getContactHelper().isThereAContact()
+                && ! app.getGroupHelper().isThereAGroup()) {
+            app.getNavigationHelper().gotoGroupPage();
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+            app.getNavigationHelper().gotoHomePage();
+            app.getContactHelper().createContact(
+                    new ContactData(
+                            "Ivan",
+                            "Ivanov",
+                            "My Company",
+                            "My Address",
+                            "My home telephone",
+                            "my_email@gmail.com",
+                            "test1"));
+        }
+        app.getNavigationHelper().gotoHomePage();
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("Ivan", "Ivanov", "My Company", "My Address",
                 "My home telephone", "my_email@gmail.com", null), false);
