84f009254837a1b08d7069bc86e2ea5e4e56b9fe
human122 <zorry@rambler.ru>
2022-01-08 16:33:10 +0400

    Описание тестового стенда перенесено в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -35,34 +35,36 @@ public class ContactCreationTests extends TestBase {
   @DataProvider
   public Iterator<Object[]> ValidContactsFromXml() throws IOException {
     File photo = new File("src/test/resources/desert.png");
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null) {
-      xml += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))){
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((c) -> new Object[] {c.withGroup("[none]").withPhoto(photo)})
+              .collect(Collectors.toList()).iterator();
     }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(ContactData.class);
-    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-    return contacts.stream().map((c) -> new Object[] {c.withGroup("[none]").withPhoto(photo)})
-            .collect(Collectors.toList()).iterator();
   }
 
   @DataProvider
   public Iterator<Object[]> ValidContactsFromJson() throws IOException {
     File photo = new File("src/test/resources/desert.png");
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
-    String json = "";
-    String line = reader.readLine();
-    while (line != null) {
-      json += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))){
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+      return contacts.stream().map((c) -> new Object[] {c.withGroup("[none]").withPhoto(photo)})
+              .collect(Collectors.toList()).iterator();
     }
-    Gson gson = new Gson();
-    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
-    return contacts.stream().map((c) -> new Object[] {c.withGroup("[none]").withPhoto(photo)})
-            .collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "ValidContactsFromJson")
